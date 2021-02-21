b848755ad84924b9c6aa17044b7fe5d557987e16
daniel012 <oliveros.jesusd@gmail.com>
2021-02-21 06:47:11 +0000

    connection mysql and api

--- application/controllers/__init__.py
+++ application/controllers/__init__.py
@@ -1,12 +1,29 @@
-import flask
+from flask import Flask, render_template, json, request
+from flaskext.mysql import MySQL
 
-app = flask.Flask(__name__)
-app.config["DEBUG"] = True
+mysql = MySQL()
+app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'admin_sftp'
+app.config['MYSQL_DATABASE_PASSWORD'] = '89Qftp_sql'
+app.config['MYSQL_DATABASE_DB'] = 'transferencia_sise'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>hola mamor</h1><p>soy un srvicio web.</p>"
+ conn = mysql.connect()
+ cursor =conn.cursor()
+ cursor.execute('SELECT * FROM servidor')
+ rv = cursor.fetchall()
+ row_headers=[x[0] for x in cursor.description]
+ json_data=[]
+ for result in rv:
+  json_data.append(dict(zip(row_headers,result)))
+ return json.dumps(json_data)
+
 
 app.run(debug=True,host='0.0.0.0')
 
